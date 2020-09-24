bbf8f72514288b999c6be893a90d3077cb927fe8
Thiago P <thiagop@usp.br>
2020-09-23 22:36:05 -0300

    changed gitignore# Please enter the commit message for your changes. Lines starting

---
+++ pdfparser/parser_test.go
@@ -0,0 +1,32 @@
+package pdfparser
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestReadPDF(t *testing.T) {
+	body, ok := ReadPDF("historico.pdf")
+
+	if ok != true {
+		t.Error("An error ocurred")
+	} else {
+		fmt.Println(*body)
+	}
+}
+
+func TestParsePDF(t *testing.T) {
+	body, ok := ReadPDF("historico.pdf")
+
+	if ok != true {
+		t.Error("Error reading PDF")
+	}
+
+	results, ok := ParsePDF(body)
+
+	if ok != true {
+		t.Error("Error parsing PDF")
+	} else {
+		fmt.Println(results)
+	}
+}
