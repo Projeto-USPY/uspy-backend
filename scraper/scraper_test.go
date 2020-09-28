a5c07e6c5ebbb0a408f36fd3e30e6cd55c0c800d
Thiago P <thiagop@usp.br>
2020-09-27 21:06:50 -0300

    removed test that doesnt make sense anymore

--- scraper/scraper_test.go
+++ scraper/scraper_test.go
@@ -9,27 +9,6 @@ func TestScrapeDepartments(t *testing.T) {
 	result := ScrapeDepartments()
 	fmt.Println(result)
 }
-
-/*
-func TestScrapeSubjectDescription(t *testing.T) {
-	var course string = "SCC0200"
-	desc, err := ScrapeSubjectDescription(course)
-	if err != nil {
-		t.Error(err)
-	} else {
-		t.Log(desc)
-	}
-
-	course = "SCC1234"
-	desc, err = ScrapeSubjectDescription(course)
-	if err != nil {
-		t.Log(err)
-	} else {
-		t.Fail()
-	}
-}
-*/
-
 func TestScrapeICMC(t *testing.T) {
 	courses, err := ScrapeICMC()
 
