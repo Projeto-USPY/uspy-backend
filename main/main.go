bdcb6a7ac79bf86fe2dc9a6034e152bcc0c7e618
Lucas Turci <lucas.turci@gmail.com>
2020-12-08 23:13:14 -0300

    Implement reading of JSON Files
    - Implement LoadData function in server.go. It reads the JSON files from /data and loads them to `courses` and `professors`, two global variables
    - Change REST skeleton to deliver the data
    - TODO: add some tests?

---
+++ main/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/tpreischadt/ProjetoJupiter/server"
+)
+
+func main() {
+	r := gin.Default()
+	r.NoRoute(server.DefaultPage)
+
+	api := r.Group("/api")
+
+	err := server.LoadData()
+	if err != nil {
+		panic(err)
+	}
+
+	// PROFESSORS --------------------------------------------------
+	{
+		professorAPI := api.Group("/professor")
+
+		professorAPI.GET("/:id", func(c *gin.Context) {
+			id := c.Param("id")
+
+			if id == "all" {
+				res := server.GetProfessors()
+				c.JSON(http.StatusOK, res)
+			} else {
+				prof := server.GetProfessorByID(id)
+				c.JSON(http.StatusOK, prof)
+			}
+		})
+	}
+
+	// SUBJECTS ---------------------------------------------
+	{
+		subjectAPI := api.Group("/subject")
+
+		subjectAPI.GET("/:code", func(c *gin.Context) {
+			code := c.Param("code")
+
+			if code == "all" {
+				res := server.GetSubjects()
+				c.JSON(http.StatusOK, res)
+			} else {
+				subject := server.GetSubjectByCode(code)
+				c.JSON(http.StatusOK, subject)
+			}
+		})
+	}
+
+	r.Run()
+}
--- server/server.go
+++ server/server.go
@@ -5,11 +5,54 @@ package server
 */
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/tpreischadt/ProjetoJupiter/scraper"
 )
+
+// Global variables to store JSON Data
+var courses []scraper.Course
+var professors []scraper.Professor
+
+// Responsible for reading JSON Files and loading to memory
+func LoadData() error {
+
+	// I dont know exactly what path makes most sense in our future docker environment, so *rethink later*
+	const coursesJSONFileName = "../data/courses.json"
+	const professorsJSONFileName = "../data/professors.json"
+
+	// Read Courses
+	coursesJSONFile, err := ioutil.ReadFile(coursesJSONFileName)
+
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal([]byte(coursesJSONFile), &courses)
+
+	if err != nil {
+		return err
+	}
+
+	// Read Professors
+	professorsJSONFile, err := ioutil.ReadFile(professorsJSONFileName)
+
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal([]byte(professorsJSONFile), &professors)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 // Todo (return default page)
 func DefaultPage(c *gin.Context) {
@@ -18,7 +61,7 @@ func DefaultPage(c *gin.Context) {
 
 // GetProfessors returns list of all professors at every department
 func GetProfessors() []scraper.Professor {
-	return make([]scraper.Professor, 0)
+	return professors
 }
 
 // GetProfessorByDepartment returns list of all professors at department 'dep'
@@ -28,6 +71,11 @@ func GetProfessorByDepartment(dep string) []scraper.Professor {
 
 // GetProfessorByID returns Professor with id 'id'
 func GetProfessorByID(id string) scraper.Professor {
+	for _, professor := range professors {
+		if strconv.Itoa(professor.ID) == id {
+			return professor
+		}
+	}
 	return scraper.Professor{}
 }
 
@@ -38,5 +86,12 @@ func GetSubjects() []scraper.Subject {
 
 // GetSubjectByCode returns Subject with code 'code'
 func GetSubjectByCode(code string) scraper.Subject {
+	for _, course := range courses {
+		for _, subject := range course.Subjects {
+			if subject.Code == code {
+				return subject
+			}
+		}
+	}
 	return scraper.Subject{}
 }
