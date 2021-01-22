b3b2b859a30fb35a05169fc0b95fb8abc6e67abc
preischadt <thiagop@usp.br>
2021-01-22 15:54:29 -0300

    add error checking in case router setup fails

--- main/main.go
+++ main/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"github.com/tpreischadt/ProjetoJupiter/server"
+	"log"
 	"os"
 )
 
 func main() {
-	r := server.SetupRouter(server.SetupDB(".env"))
+	r, err := server.SetupRouter(server.SetupDB(".env"))
+	if err != nil {
+		log.Fatal(err)
+	}
 	_ = r.Run(os.Getenv("DOMAIN") + ":" + os.Getenv("PORT"))
 }
