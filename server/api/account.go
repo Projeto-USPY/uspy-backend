4d50462976152e72e6c8184da0b45165a5baf936
preischadt <thiagop@usp.br>
2021-01-22 11:29:37 -0300

    add basic signup logic and change secure cookie header according to router mode
    if in production, use Secure Cookies (HTTPS)

--- server/api/account.go
+++ server/api/account.go
@@ -4,6 +4,8 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/tpreischadt/ProjetoJupiter/db"
 	"github.com/tpreischadt/ProjetoJupiter/entity"
+	"github.com/tpreischadt/ProjetoJupiter/pdfparser"
+	iddigital "github.com/tpreischadt/ProjetoJupiter/pdfparser/auth"
 	"github.com/tpreischadt/ProjetoJupiter/server/auth"
 	"net/http"
 	"os"
@@ -25,16 +27,73 @@ func Login(DB db.Env) func(c *gin.Context) {
 		} else {
 			domain := os.Getenv("DOMAIN")
 
-			// expiration date = 1 month
-			c.SetCookie("access_token", jwt, 30*24*3600, "/", domain, false, true)
+			// expiration date = 1 monthFirestore
+			secureCookie := os.Getenv("MODE") == "prod"
+			c.SetCookie("access_token", jwt, 30*24*3600, "/", domain, secureCookie, true)
 			c.Status(http.StatusOK)
 		}
 	}
 }
 
-// TODO
 func Signup(DB db.Env) func(g *gin.Context) {
 	return func(c *gin.Context) {
+		var signupForm entity.Signup
+		if err := c.ShouldBindJSON(&signupForm); err != nil {
+			c.Status(http.StatusBadRequest)
+			return
+		}
+
+		cookies := c.Request.Cookies()
+		resp, err := iddigital.PostAuthCode(signupForm.AccessKey, signupForm.Captcha, cookies)
+		if err != nil {
+			// error getting PDF from iddigital
+			c.Status(http.StatusInternalServerError)
+			return
+		} else if resp.Header.Get("Content-Type") != "application/pdf" {
+			// wrong captcha or auth
+			c.Status(http.StatusBadRequest)
+			return
+		}
+
+		if pdf, ok := pdfparser.ReadPDFResponse(resp); !ok {
+			// error converting PDF to text
+			c.Status(http.StatusInternalServerError)
+		} else {
+			data, ok := pdfparser.ParsePDF(pdf)
+			if !ok {
+				// error parsing pdf
+				c.Status(http.StatusInternalServerError)
+				return
+			}
+			// TODO: check if grades timestamp and add user to firestore
+			c.JSON(http.StatusOK, data)
+		}
+	}
+}
+
+func SignupCaptcha() func(c *gin.Context) {
+	return func(c *gin.Context) {
+		resp, err := iddigital.GetCaptcha()
+		if err != nil || resp.StatusCode != http.StatusOK {
+			c.Status(http.StatusInternalServerError)
+			return
+		}
+		defer resp.Body.Close()
+
+		cookies := resp.Cookies()
+		for _, ck := range cookies {
+			domain := os.Getenv("DOMAIN")
+			secureCookie := os.Getenv("MODE") == "prod"
+			c.SetCookie(ck.Name, ck.Value, ck.MaxAge, "/account/create", domain, secureCookie, ck.HttpOnly)
+		}
+
+		c.DataFromReader(
+			http.StatusOK,
+			resp.ContentLength,
+			resp.Header.Get("Content-Type"),
+			resp.Body,
+			map[string]string{},
+		)
 
 	}
 }
