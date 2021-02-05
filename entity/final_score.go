b4a2cfa0c64a9c77364aed9848f139b2b64c8849
Thiago <thiagop@usp.br>
2021-02-05 12:09:41 -0300

    add year and semester to final_score hash

--- entity/final_score.go
+++ entity/final_score.go
@@ -1,14 +1,23 @@
 package entity
 
 import (
+	"crypto/md5"
+	"fmt"
 	"github.com/tpreischadt/ProjetoJupiter/db"
 )
 
 type FinalScore struct {
 	Grade        float64 `firestore:"grade"`
 	Status       string  `firestore:"status"`
+	Year         int     `firestore:"-"`
+	Semester     int     `firestore:"-"`
 	Frequency    int     `firestore:"frequency"`
 	OfferingHash string  `firestore:"offering,omitempty"`
+}
+
+func (mf FinalScore) Hash() string {
+	str := fmt.Sprintf("%d%d", mf.Year, mf.Semester)
+	return fmt.Sprintf("%x", md5.Sum([]byte(str)))
 }
 
 func (mf FinalScore) Insert(DB db.Env, collection string) error {
