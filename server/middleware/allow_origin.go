2d607b6faf9d2ea881b13d565448a34ca628f8b3
Lucas Turci <lucas.turci@gmail.com>
2021-02-05 14:25:18 -0300

    Add more headers to allow_origin middleware and also resolve preflight requests

--- server/middleware/allow_origin.go
+++ server/middleware/allow_origin.go
@@ -7,6 +7,16 @@ import (
 // AllowAnyOrigin enables CORS for testing purposes
 func AllowAnyOrigin() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "*")
-	}
+                c.Header("Access-Control-Allow-Origin", "*")
+                c.Header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS")
+                c.Header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With,observe")
+
+                if c.Request.Method == "OPTIONS" {
+                        c.AbortWithStatus(204)
+                        return
+                }
+
+                c.Next()
+        }
+
 }
