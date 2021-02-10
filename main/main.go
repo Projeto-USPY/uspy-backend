b6becf43538cb9116bf4844c69f4da92a5ec20bc
Thiago <thiagop@usp.br>
2021-02-10 20:14:21 -0300

    add building option to main using .env MODE

--- main/main.go
+++ main/main.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"github.com/tpreischadt/ProjetoJupiter/builder"
 	"github.com/tpreischadt/ProjetoJupiter/db"
 	"github.com/tpreischadt/ProjetoJupiter/server"
 	"log"
@@ -9,9 +10,22 @@ import (
 )
 
 func main() {
-	r, err := server.SetupRouter(db.SetupDB(".env"))
+	DB := db.SetupDB(".env")
+	r, err := server.SetupRouter(DB)
+
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	if s, _ := os.LookupEnv("MODE"); s == "build" {
+		for _, b := range builder.Builders {
+			err := b.Build(DB)
+			if err != nil {
+				log.Fatal("failed to build: ", err)
+			}
+		}
+		return
+	}
+
 	_ = r.Run(os.Getenv("DOMAIN") + ":" + os.Getenv("PORT"))
 }
