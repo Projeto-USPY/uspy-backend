d32ddf9ac36e21955bf7d72190230cd2134735d0
Thiago <thiagop@usp.br>
2021-03-15 22:28:26 -0300

    start test refactor

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/PuerkitoBio/goquery v1.5.1
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.6.3
+	github.com/go-playground/assert/v2 v2.0.1
 	github.com/go-playground/validator/v10 v10.4.1
 	github.com/joho/godotenv v1.3.0
 	github.com/json-iterator/go v1.1.10 // indirect
---
+++ scraper/subject_test.go
@@ -0,0 +1,68 @@
+package scraper
+
+import (
+	"github.com/go-playground/assert/v2"
+	"github.com/tpreischadt/ProjetoJupiter/entity"
+	"testing"
+)
+
+type TestInput struct {
+	subject        string
+	course         string
+	specialization string
+}
+
+var testCases = []struct {
+	input    TestInput
+	expected entity.Subject
+}{
+	{
+		input: TestInput{
+			subject:        "SCC0230",
+			course:         "55090",
+			specialization: "0",
+		},
+		expected: entity.Subject{
+			Code:           "SCC0230",
+			CourseCode:     "55090",
+			Specialization: "0",
+			Name:           "Inteligência Artificial",
+			Description:    "Apresentar ao aluno as idéias fundamentais da Inteligência Artificial e algumas características relacionadas à implementação desse tipo de sistemas.",
+			ClassCredits:   4,
+			AssignCredits:  1,
+			TotalHours:     "90 h",
+			Stats:          map[string]int{"total": 0, "worth_it": 0},
+		},
+	},
+	{
+		input: TestInput{
+			subject:        "SCC0222",
+			course:         "55041",
+			specialization: "0",
+		},
+		expected: entity.Subject{
+			Code:           "SCC0222",
+			CourseCode:     "55041",
+			Specialization: "0",
+			Name:           "Laboratório de Introdução à Ciência de Computação I",
+			Description:    "Implementar em laboratório as técnicas de programação apresentadas em Introdução à Ciência da Computação I, utilizando uma linguagem de programação estruturada.",
+			ClassCredits:   2,
+			AssignCredits:  2,
+			TotalHours:     "90 h",
+			Stats:          map[string]int{"total": 0, "worth_it": 0},
+		},
+	},
+}
+
+func TestNewSubjectScraper(t *testing.T) {
+	for _, tc := range testCases {
+		sc := NewSubjectScraper(tc.input.subject, tc.input.course, tc.input.specialization)
+
+		if sub, err := sc.Start(); err != nil {
+			t.Fatal(err)
+		} else {
+			assert.Equal(t, sub.(entity.Subject), tc.expected)
+		}
+
+	}
+}
