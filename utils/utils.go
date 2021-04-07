37f6b49c24081571223ba6332c0aa92d0b248e89
Thiago <thiagop@usp.br>
2021-04-07 20:01:13 -0300

    separate utils scraper functions to another repository

--- utils/utils.go
+++ utils/utils.go
@@ -6,92 +6,9 @@ import (
 	"crypto/cipher"
 	"crypto/rand"
 	"encoding/hex"
-	"encoding/json"
 	"fmt"
-	"golang.org/x/net/html/charset"
 	"io"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"net/url"
 )
-
-const (
-	JupiterURL = "https://uspdigital.usp.br/jupiterweb/"
-)
-
-// LoadJSON loads json file into data interface
-func LoadJSON(filename string, into interface{}) (err error) {
-	bytes, err := ioutil.ReadFile(filename)
-
-	if err != nil {
-		return err
-	}
-
-	err = json.Unmarshal([]byte(bytes), into)
-
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// GenerateJSON creates json file inside given folder from data struct
-func GenerateJSON(data interface{}, folder string, filename string) error {
-	bytes, err := json.MarshalIndent(&data, "", "\t")
-
-	if err != nil {
-		return err
-	}
-
-	_ = ioutil.WriteFile(folder+filename, bytes, 0644)
-	return nil
-}
-
-func CheckPanic(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func CheckResponse(res *http.Response) {
-	if res.StatusCode != http.StatusOK {
-		log.Fatalf("Status code error: %d %s\n", res.StatusCode, res.Status)
-	}
-}
-
-// Returns HTTP response and io.Reader from http.Get, which should substitute http.Body, so characters are read with UTF-8 encoding
-// Already panics if error, remember to close response.Body
-func HTTPGetWithUTF8(client *http.Client, url string) (*http.Response, io.Reader, error) {
-	resp, err := client.Get(url)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	reader, err := charset.NewReader(resp.Body, resp.Header["Content-Type"][0])
-	if err != nil {
-		return nil, nil, err
-	}
-
-	return resp, reader, nil
-}
-
-// Returns HTTP response and io.Reader from http.Post, which should substitute http.Body, so characters are read with UTF-8 encoding
-// Already panics if error, remember to close response.Body
-func HTTPPostWithUTF8(client *http.Client, url string, values url.Values) (*http.Response, io.Reader, error) {
-	resp, err := client.PostForm(url, values)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	reader, err := charset.NewReader(resp.Body, resp.Header["Content-Type"][0])
-	if err != nil {
-		return nil, nil, err
-	}
-
-	return resp, reader, nil
-}
 
 // Encrypts using AES. keyString must be 128, 196 or 256 bits.
 func AESEncrypt(stringToEncrypt string, keyString string) (encryptedString string, err error) {
