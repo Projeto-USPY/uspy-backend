1746b8ded62925014b18a5abbb4dea4b0aaacb2a
Thiago Pinheiro <thiagop@usp.br>
2021-05-06 18:19:40 -0300

    fix controller tests

--- server/controllers/account/account_test.go
+++ server/controllers/account/account_test.go
@@ -1,15 +1,12 @@
 package account_test
 
 import (
-	"bytes"
-	"encoding/json"
 	"net/http"
 	"testing"
 
 	"github.com/Projeto-USPY/uspy-backend/db"
-	emulator "github.com/Projeto-USPY/uspy-backend/emulator"
-	"github.com/Projeto-USPY/uspy-backend/server"
 	"github.com/Projeto-USPY/uspy-backend/utils"
+	"github.com/Projeto-USPY/uspy-backend/utils/test"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/suite"
 )
@@ -21,48 +18,16 @@ type AccountSuite struct {
 	accessToken *http.Cookie
 }
 
-// SetupSuite runs before suite (fetches the emulator)
-func (s *AccountSuite) SetupSuite() {
-	s.DB = emulator.MustGet()
-
-	// setup router
-	var err error
-	s.router, err = server.SetupRouter(s.DB)
-	s.Assertions.Nil(err)
-
-	// get valid AccessToken
-	s.SetupAccessToken()
-}
-
-// SetupAccessToken fetches the jwt token used for private and restricted tests
-func (s *AccountSuite) SetupAccessToken() {
-	// Login data
-	jsonBody := map[string]interface{}{"login": "123456789", "pwd": "r4nd0mpass123!@#", "remember": true}
-	payloadBuf := new(bytes.Buffer)
-	json.NewEncoder(payloadBuf).Encode(jsonBody)
-
-	// Execute login
-	w := utils.MakeRequest(s.router, http.MethodPost, "/account/login", payloadBuf)
-	s.Equal(http.StatusOK, w.Code, "status should be 200, login is correct")
-	s.GreaterOrEqual(len(w.Result().Cookies()), 1)
-
-	// Fetch returned cookie
-	s.accessToken = w.Result().Cookies()[0]
+// SetupTest runs before every test
+func (s *AccountSuite) SetupTest() {
+	s.DB, s.router, s.accessToken = test.MustGetEnvironment()
 }
 
 func TestAccountSuite(t *testing.T) {
 	suite.Run(t, new(AccountSuite))
 }
 
-func (s *AccountSuite) TestAccount() {
-	s.Run("profile", s.profile)
-	s.Run("signupCaptcha", s.signupCaptcha)
-	s.Run("changePassword", s.changePassword)
-	s.Run("logout", s.logout)
-	s.Run("delete", s.delete)
-}
-
-func (s *AccountSuite) profile() {
+func (s *AccountSuite) TestProfile() {
 	// Make request without any access token
 	w := utils.MakeRequest(s.router, http.MethodGet, "/account/profile", nil)
 	s.Equal(http.StatusUnauthorized, w.Result().StatusCode, "status should be 401 because there is no jwt")
@@ -72,13 +37,13 @@ func (s *AccountSuite) profile() {
 	s.Equal(http.StatusOK, w.Result().StatusCode, "profile should return 200 because we have the login cookie")
 }
 
-func (s *AccountSuite) signupCaptcha() {
+func (s *AccountSuite) TestSignupCaptcha() {
 	w := utils.MakeRequest(s.router, http.MethodGet, "/account/captcha", nil)
 
 	s.Equal(http.StatusOK, w.Result().StatusCode)
 	s.Equal([]string{"image/png"}, w.Result().Header["Content-Type"])
 }
 
-func (s *AccountSuite) changePassword() {}
-func (s *AccountSuite) logout()         {}
-func (s *AccountSuite) delete()         {}
+func (s *AccountSuite) TestChangePassword() {}
+func (s *AccountSuite) TestLogout()         {}
+func (s *AccountSuite) TestDelete()         {}
--- server/controllers/private/user_test.go
+++ server/controllers/private/user_test.go
@@ -1,15 +1,11 @@
 package private_test
 
 import (
-	"bytes"
-	"encoding/json"
 	"net/http"
 	"testing"
 
 	"github.com/Projeto-USPY/uspy-backend/db"
-	emulator "github.com/Projeto-USPY/uspy-backend/emulator"
-	"github.com/Projeto-USPY/uspy-backend/server"
-	"github.com/Projeto-USPY/uspy-backend/utils"
+	"github.com/Projeto-USPY/uspy-backend/utils/test"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/suite"
 )
@@ -21,45 +17,15 @@ type UserSuite struct {
 	accessToken *http.Cookie
 }
 
-// SetupSuite runs before suite (fetches the emulator)
-func (s *UserSuite) SetupSuite() {
-	s.DB = emulator.MustGet()
-
-	// setup router
-	var err error
-	s.router, err = server.SetupRouter(s.DB)
-	s.Assertions.Nil(err)
-
-	// get valid AccessToken
-	s.SetupAccessToken()
-}
-
-// SetupAccessToken fetches the jwt token used for private and restricted tests
-func (s *UserSuite) SetupAccessToken() {
-	// Login data
-	jsonBody := map[string]interface{}{"login": "123456789", "pwd": "r4nd0mpass123!@#", "remember": true}
-	payloadBuf := new(bytes.Buffer)
-	json.NewEncoder(payloadBuf).Encode(jsonBody)
-
-	// Execute login
-	w := utils.MakeRequest(s.router, http.MethodPost, "/account/login", payloadBuf)
-	s.Equal(http.StatusOK, w.Code, "status should be 200, login is correct")
-	s.GreaterOrEqual(len(w.Result().Cookies()), 1)
-
-	// Fetch returned cookie
-	s.accessToken = w.Result().Cookies()[0]
+// SetupTest runs before every test
+func (s *UserSuite) SetupTest() {
+	s.DB, s.router, s.accessToken = test.MustGetEnvironment()
 }
 
 func TestUserSuite(t *testing.T) {
 	suite.Run(t, new(UserSuite))
 }
 
-func (s *UserSuite) TestUser() {
-	s.Run("getGrade", s.getGrade)
-	s.Run("getSubjectReview", s.getSubjectReview)
-	s.Run("updateSubjectReview", s.updateSubjectReview)
-}
-
-func (s *UserSuite) getGrade()            {}
-func (s *UserSuite) getSubjectReview()    {}
-func (s *UserSuite) updateSubjectReview() {}
+func (s *UserSuite) TestGetGrade()            {}
+func (s *UserSuite) TestGetSubjectReview()    {}
+func (s *UserSuite) TestUpdateSubjectReview() {}
