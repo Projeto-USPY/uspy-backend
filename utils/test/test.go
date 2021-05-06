4307fe863901dfa2f4a80325880c0ed4a73f2e99
Thiago Pinheiro <thiagop@usp.br>
2021-05-06 18:17:14 -0300

    make more reusable test setup functions

--- utils/test/test.go
+++ utils/test/test.go
@@ -6,12 +6,15 @@ import (
 	"errors"
 	"net/http"
 
+	"github.com/Projeto-USPY/uspy-backend/db"
+	"github.com/Projeto-USPY/uspy-backend/server"
 	"github.com/Projeto-USPY/uspy-backend/utils"
+	"github.com/Projeto-USPY/uspy-backend/utils/test/emulator"
 	"github.com/gin-gonic/gin"
 )
 
-// SetupAccessToken fetches the jwt token used for private and restricted tests
-func SetupAccessToken(router *gin.Engine) (*http.Cookie, error) {
+// setupAccessToken fetches the jwt token used for private and restricted tests
+func setupAccessToken(router *gin.Engine) (*http.Cookie, error) {
 	// Login data
 	jsonBody := map[string]interface{}{"login": "123456789", "pwd": "r4nd0mpass123!@#", "remember": true}
 	payloadBuf := new(bytes.Buffer)
@@ -26,4 +29,27 @@ func SetupAccessToken(router *gin.Engine) (*http.Cookie, error) {
 
 	// Fetch returned cookie
 	return w.Result().Cookies()[0], nil
+}
+
+// GetEnvironment will reinitialize the testing environment.
+// It requires a suite because it is meant to be run with suites.
+func MustGetEnvironment() (DB db.Env, router *gin.Engine, cookie *http.Cookie) {
+	DB = emulator.MustGet()
+	if err := emulator.Setup(DB); err != nil {
+		panic(err)
+	}
+
+	// setup router
+	var err error
+	router, err = server.SetupRouter(DB)
+	if err != nil {
+		panic(err)
+	}
+
+	// get valid AccessToken
+	if cookie, err = setupAccessToken(router); err != nil {
+		panic(err)
+	}
+
+	return
 }
