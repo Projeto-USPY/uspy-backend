0453792906257817f77cfd6caf538f35aefeeec3
Thiago Pinheiro <thiagop@usp.br>
2021-05-07 00:12:48 -0300

    add change password test

--- server/controllers/account/account_test.go
+++ server/controllers/account/account_test.go
@@ -1,6 +1,7 @@
 package account_test
 
 import (
+	"bytes"
 	"net/http"
 	"testing"
 
@@ -44,6 +45,49 @@ func (s *AccountSuite) TestSignupCaptcha() {
 	s.Equal([]string{"image/png"}, w.Result().Header["Content-Type"])
 }
 
-func (s *AccountSuite) TestChangePassword() {}
-func (s *AccountSuite) TestLogout()         {}
-func (s *AccountSuite) TestDelete()         {}
+func (s *AccountSuite) TestChangePassword() {
+	loginBody := `
+		{
+			"login": "123456789",
+			"pwd": "r4nd0mpass123!@#"
+		}
+	`
+
+	newLoginBody := `
+	{
+		"login": "123456789",
+		"pwd": "p4ssw0rdr4nd0m123!@#"
+	}
+	`
+
+	incorrectBody := `
+		{
+			"old_password": "wr0ngpass123!@#",
+			"new_password": "p4ssw0rdr4nd0m123!@#"
+		}
+	`
+
+	changePwdBody := `
+		{
+			"old_password": "r4nd0mpass123!@#",
+			"new_password": "p4ssw0rdr4nd0m123!@#"
+		}
+	`
+
+	w := utils.MakeRequest(s.router, http.MethodPost, "/account/login", bytes.NewBuffer([]byte(loginBody)))
+	s.Equal(http.StatusOK, w.Result().StatusCode, "failed to login with original credentials")
+
+	w = utils.MakeRequest(s.router, http.MethodPut, "/account/password_change", bytes.NewBuffer([]byte(incorrectBody)), s.accessToken)
+	s.Equal(http.StatusForbidden, w.Result().StatusCode, "changed password with wrong old pass")
+
+	w = utils.MakeRequest(s.router, http.MethodPut, "/account/password_change", bytes.NewBuffer([]byte(changePwdBody)), s.accessToken)
+	s.Equal(http.StatusOK, w.Result().StatusCode, "failed to change password")
+
+	w = utils.MakeRequest(s.router, http.MethodPost, "/account/login", bytes.NewBuffer([]byte(loginBody)))
+	s.Equal(http.StatusUnauthorized, w.Result().StatusCode, "managed to login with old credentials")
+
+	w = utils.MakeRequest(s.router, http.MethodPost, "/account/login", bytes.NewBuffer([]byte(newLoginBody)))
+	s.Equal(http.StatusOK, w.Result().StatusCode, "failed to login with new credentials")
+}
+func (s *AccountSuite) TestLogout() {}
+func (s *AccountSuite) TestDelete() {}
