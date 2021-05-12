eb9de3ec21ffe8e4c5ffa9bb0ea8c9a73a3c89be
Thiago Pinheiro <thiagop@usp.br>
2021-05-11 21:57:24 -0300

    add professors to institute

--- entity/institute.go
+++ entity/institute.go
@@ -8,6 +8,8 @@ type Institute struct {
 	Name    string   `json:"name"`
 	Code    string   `json:"code"`
 	Courses []Course `json:"courses"`
+
+	Professors []Professor `json:"professors"`
 }
 
 func (i Institute) Insert(DB db.Env, collection string) error { return nil }
