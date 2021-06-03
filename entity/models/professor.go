3f56f8e4705162a0c53837291657e56cf8f2f86c
Thiago Pinheiro <thiagop@usp.br>
2021-06-03 01:43:59 -0300

    add dummy methods to allow professor to be a writer

--- entity/models/professor.go
+++ entity/models/professor.go
@@ -1,8 +1,18 @@
 package models
 
+import "github.com/Projeto-USPY/uspy-backend/db"
+
 type Professor struct {
 	CodPes string
 	Name   string
 
 	Offerings []Offering
+}
+
+func (prof Professor) Insert(DB db.Env, collection string) error {
+	return nil
+}
+
+func (prof Professor) Update(DB db.Env, collection string) error {
+	return nil
 }
