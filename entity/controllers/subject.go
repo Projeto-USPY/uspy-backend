c0f3331194b0d810461d467ca8e104a017fa40a9
Thiago Pinheiro <thiagop@usp.br>
2021-06-06 17:39:06 -0300

    add query parameter limit to subject

--- entity/controllers/subject.go
+++ entity/controllers/subject.go
@@ -4,4 +4,6 @@ type Subject struct {
 	Code           string `form:"code" binding:"required,alphanum"`
 	CourseCode     string `form:"course" binding:"required,alphanum"`
 	Specialization string `form:"specialization" binding:"required,alphanum"`
+
+	Limit int `form:"limit"`
 }
