849d450f77aad201def54de0139d2fbeb33e8a75
Thiago Pinheiro <thiagop@usp.br>
2021-06-11 00:45:23 -0300

    change so embedded struct can be implicit

--- entity/controllers/offering.go
+++ entity/controllers/offering.go
@@ -1,6 +1,6 @@
 package controllers
 
 type Offering struct {
-	Subject Subject
-	Hash    string `form:"professor" binding:"required,len=64,alphanum"` // sha256
+	Subject
+	Hash string `form:"professor" binding:"required,len=64,alphanum"` // sha256
 }
