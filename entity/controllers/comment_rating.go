9d0f981254b3ed61244d5f788efd63df1bca2084
Thiago Pinheiro <thiagop@usp.br>
2021-06-11 00:43:17 -0300

    use one structure for report and rating endpoints

--- entity/controllers/comment_rating.go
+++ entity/controllers/comment_rating.go
@@ -1,7 +1,14 @@
 package controllers
 
 type CommentRating struct {
-	Offering Offering
-	Comment  string `form:"comment" binding:"required,uuid"`
-	IsUpvote bool   `json:"upvote" binding:"required"`
+	Offering
+	Comment string `form:"comment" binding:"required,uuid"`
+}
+
+type CommentVoteBody struct {
+	IsUpvote bool `json:"upvote" binding:"required"`
+}
+
+type CommentReportBody struct {
+	Body string `json:"body" binding:"required,min=10,max=300"`
 }
--- entity/controllers/comment_report.go
+++
@@ -1,7 +0,0 @@
-package controllers
-
-type CommentReport struct {
-	Offering Offering
-	Comment  string `form:"comment" binding:"required,uuid"`
-	Body     string `json:"body" binding:"required,min=10,max=300"`
-}
