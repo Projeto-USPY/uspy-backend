17fda6d15123b7363ec0abbbba18cc3e2bbdace5
Thiago Pinheiro <thiagop@usp.br>
2021-06-12 19:13:42 -0300

    add constructor to comment rating view

--- entity/views/comment_rating.go
+++ entity/views/comment_rating.go
@@ -1,5 +1,11 @@
 package views
 
+import "github.com/Projeto-USPY/uspy-backend/entity/models"
+
 type CommentRating struct {
 	IsUpvote bool `json:"upvote"`
+}
+
+func NewCommentRatingFromModel(model *models.CommentRating) *CommentRating {
+	return &CommentRating{IsUpvote: model.Upvote}
 }
