e5a5f9bf6c8f428c95673031624bbcb92934ffd7
tprei <thiago.preischadt@gmail.com>
2021-08-23 19:14:48 -0300

    update controller entities

--- entity/controllers/signup_form.go
+++ entity/controllers/signup_form.go
@@ -4,4 +4,5 @@ type SignupForm struct {
 	AccessKey string `json:"access_key" binding:"required,validateAccessKey"`
 	Password  string `json:"password" binding:"required,validatePassword"`
 	Captcha   string `json:"captcha" binding:"required,alphanum,len=4"`
+	Email     string `json:"email" binding:"required,email,validateEmail"`
 }
---
+++ entity/validation/account_verification.go
@@ -0,0 +1,35 @@
+package validation
+
+import (
+	"time"
+
+	"github.com/Projeto-USPY/uspy-backend/config"
+	"github.com/Projeto-USPY/uspy-backend/utils"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/go-playground/validator/v10"
+)
+
+func validateVerificationToken(f1 validator.FieldLevel) bool {
+	str := f1.Field().String()
+	if token, err := utils.ValidateJWT(str, config.Env.JWTSecret); err != nil {
+		return false
+	} else {
+		claims := token.Claims.(jwt.MapClaims)
+
+		// assert correct operation
+		if operation, ok := claims["type"].(string); !ok {
+			return false
+		} else if operation != "email_verification" {
+			return false
+		}
+
+		// assert timestamp is less than an hour old
+		if creationDate, ok := claims["timestamp"].(string); !ok {
+			return false
+		} else if t, err := time.Parse(time.RFC3339Nano, creationDate); err != nil {
+			return false
+		} else {
+			return time.Now().Sub(t) < time.Hour
+		}
+	}
+}
