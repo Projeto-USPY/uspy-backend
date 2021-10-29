c19c8c748650d134f8f2cb58d3a28fcd8e72cb1b
tprei <thiago.preischadt@gmail.com>
2021-10-29 16:41:00 -0300

    change response payload from upvote to type

--- entity/views/comment_rating.go
+++ entity/views/comment_rating.go
@@ -3,9 +3,17 @@ package views
 import "github.com/Projeto-USPY/uspy-backend/entity/models"
 
 type CommentRating struct {
-	IsUpvote bool `json:"upvote"`
+	Type string `json:"type"`
 }
 
 func NewCommentRatingFromModel(model *models.CommentRating) *CommentRating {
-	return &CommentRating{IsUpvote: model.Upvote}
+	var responseType string
+
+	if model.Upvote {
+		responseType = "upvote"
+	} else {
+		responseType = "downvote"
+	}
+
+	return &CommentRating{Type: responseType}
 }
