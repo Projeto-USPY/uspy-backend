1549acd652a47dd9d72064f447bb51e49cd08b6c
tprei <thiagopreischadt@gmail.com>
2021-12-03 19:36:13 -0300

    add offering info to report and rating to allow lookup

--- entity/models/comment_rating.go
+++ entity/models/comment_rating.go
@@ -7,6 +7,11 @@ import (
 type CommentRating struct {
 	ID     uuid.UUID `firestore:"id"`
 	Upvote bool      `firestore:"upvote"`
+
+	ProfessorHash  string `firestore:"professor"`
+	Subject        string `firestore:"subject"`
+	Course         string `firestore:"course"`
+	Specialization string `firestore:"specialization"`
 }
 
 func (cr CommentRating) Hash() string {
